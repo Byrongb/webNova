e6af0c45b5a81e84d754f396843a56c1005fb180
Byron <bgguerra@scl.edu.gt>
2024-04-12 22:18:29 -0700

    se añade header, contendor 1 y 2 con imagen encima y texto sobre descuentos en la parte de abajo del segundo contenedor

---
+++ js/header.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const headerContainer = document.getElementById('header-container');
+
+    headerContainer.innerHTML = `
+    <header class="header">
+        <div class="menu-icon">
+            <img src="https://bnz07pap001files.storage.live.com/y4mgXCWrJHdC5LteRGugl99xTEKGqVnmPurwGzVE-bM_gCa1_kS6b2NvFvj6Y71BIXpg74dxSkZxcd69zu2FfDZBC4OE_eAJVglUHo7AnmiAs9-D32E6YnZJ1GQInUHrSPaBYzL4EZBtA8MaiTe6OA4BAUCb7N8PuIXpG-v657-y9sUNKBQjnkU7kjnKpmOk_PqbBNEJWY0dWXA8u6PQIBexfyfJSbuPCoX4Zu9YI9SppI?encodeFailures=1&width=476&height=375" alt="Menu Icon"> <!-- Espacio para imagen del menú -->
+        </div>
+        <div class="logo-center">
+            <img src="https://bnz07pap001files.storage.live.com/y4mRHI73ci72DwvVkqwKk9QZGt45jDCzdFwdDvkbUinlxvF57bSnpCgOSLvtpIVDmD9B_a5jBf88sXV83T7n4zZbWOXxe9nfn0U80BmGL4VL2PIdRunym5gKjUnXiSw2X3ER-KMfr1G1nol0TycWE4RcSy9hqUY8jp7lh1vV4ibFQ2UySh5usS7YDw0mUPXgaQAxM5XRi-8JDEeGbJ1xH-b7t2slt6h-33oCQDcZWFvB7c?encodeFailures=1&width=440&height=432" alt="WebNova Logo" class="logo">
+        </div>
+        <div class="buy-now">
+            <button>BUY<br>NOW</button>
+        </div>
+    </header>
+    `;
+});
---
+++ js/inicio.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const inicioContainer = document.createElement('div');
+    inicioContainer.className = 'inicio-container';
+
+    inicioContainer.innerHTML = `
+        <div class="inicio-top"></div>
+        <img src="https://bnz07pap001files.storage.live.com/y4mxHqpD0jDSF8wBuhl8tHRxtgEq2euyAyPqXQFAu36H2TLmPQ4fYPUPDCptu3TkJRDDwnpJfsMy3MCShy4WV3QqVjGanGjGixsc3xDJm_aHgpRhIMHVJsXh7QiNWJFqDcxtg4SMwyNYFyDnf6Riw-yIA8r8TZK64eOWpZB86PCbBKEiXASi3fbkLaIFrKPO4z6rkKcdEZkOD2HFupu49kcWy13dy3k3HjYxYttTzKpmII?encodeFailures=1&width=549&height=455" alt="Imagen Descriptiva" class="imagen-central">
+        <div class="inicio-bottom">
+            <div class="text-container">
+                <p class="line-text line1">─── Obten un 30% de ───</p>
+                <div class="discount-line">
+                    <p class="discount-text">DESCUENTO</p>
+                    <div class="price-box">
+                        <div class="price-info">
+                            $999
+                            <br>
+                            <s>$1500</s>
+                        </div>
+                    </div>
+                </div>
+                <p class="line-text line3">en su página web ahora</p>
+            </div>
+        </div>
+    `;
+
+    document.body.appendChild(inicioContainer);
+});
