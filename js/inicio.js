a6b463fa27c7284eb97ff37020583d5478c768c4
Byron <bgguerra@scl.edu.gt>
2024-04-17 18:33:29 -0700

    se añade el contenido del contenedor 2

--- js/inicio.js
+++ js/inicio.js
@@ -49,8 +49,10 @@ document.addEventListener('DOMContentLoaded', function() {
         const infoContainer = document.createElement('div');
         infoContainer.className = `contenedorInfo contenedorInfo${i}`;
 
-        if (i === 1) {
-            infoContainer.innerHTML = `
+        // Añadir contenido solo a los contenedores específicos
+        switch(i) {
+            case 1:
+                infoContainer.innerHTML = `
                 <h2 class="info-title">Convierte clics en clientes</h2>
                 <button class="details-button">Obtener Detalles</button>
                 <div class="more-info-container">
@@ -60,9 +62,24 @@ document.addEventListener('DOMContentLoaded', function() {
                     <button class="see-plans">VER PORTAFOLIO</button>
                     <p class="small-text">Consulte nuestros planes para más detalles sobre precios y servicios.</p>
                 </div>
-            `;
-        } else {
-            infoContainer.textContent = `Contenido del contenedorInfo${i}`;
+
+                `;
+                break;
+            case 2:
+                infoContainer.innerHTML = `
+                <h2 class="info-title2">--- Obten desde ---</h2>
+                <div class="price-highlight">$85/mes</div>
+                <p class="maintenance-description">Mantenimiento eficiente y personalizado</p>
+                <button class="details-button2">Ver Planes</button>
+                <div class="more-info-container">
+                    <img src="https://bnz07pap001files.storage.live.com/y4mazaGEryfxc4EisricxUqVevd92hSquZNSiF2Lw6h4_GsNZYyPaT-fph7OucBDqDe1zSR3PFS_Q8QFfyIXiKW1ZFG3MvcMAiD2s1zlfbyDCOZCsNvSPoVp0zDc18Fp8q0UQVq_PT5qX7fhHWpk5Ev9o0hYGrUwuq9WHtvu_KRxzW0Np-UN0F0jm3qEkwO8LhyouP1FVT5lIhz4wfwYqcSTZDgwwJ_S_mn25LlnDrQQpY?encodeFailures=1&width=543&height=486" alt="Descripción de la imagen" class="maintenance-image">
+                    <p class="info-text">Garantiza el rendimiento óptimo de tu sitio web con nuestros planes de mantenimiento personalizados.</p>
+                </div>
+                `;
+                break;
+            case 3:
+                infoContainer.textContent = 'Aquí irán tus habilidades y tecnologías que dominas.';
+                break;
         }
         
         inicioContainer.appendChild(infoContainer);
