485c4ad3fc2d482cfd43df47b32ed86adee347d4
Byron <bgguerra@scl.edu.gt>
2024-04-18 20:23:24 -0700

    se agregan las skills de webNova

--- js/inicio.js
+++ js/inicio.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         </div>
                     </div>
                 </div>
-                <p class="line-text line3">en su página web ahora</p>
+                <p class="line-text line3">en tu página web ahora</p>
             </div>
         </div>
     `;
@@ -74,12 +74,40 @@ document.addEventListener('DOMContentLoaded', function() {
                 <div class="more-info-container">
                     <img src="https://bnz07pap001files.storage.live.com/y4mazaGEryfxc4EisricxUqVevd92hSquZNSiF2Lw6h4_GsNZYyPaT-fph7OucBDqDe1zSR3PFS_Q8QFfyIXiKW1ZFG3MvcMAiD2s1zlfbyDCOZCsNvSPoVp0zDc18Fp8q0UQVq_PT5qX7fhHWpk5Ev9o0hYGrUwuq9WHtvu_KRxzW0Np-UN0F0jm3qEkwO8LhyouP1FVT5lIhz4wfwYqcSTZDgwwJ_S_mn25LlnDrQQpY?encodeFailures=1&width=543&height=486" alt="Descripción de la imagen" class="maintenance-image">
                     <p class="info-text">Garantiza el rendimiento óptimo de tu sitio web con nuestros planes de mantenimiento personalizados.</p>
+                    <p class="small-text2">Compra 4 meses y obten 2 meses GRATIS.</p>
                 </div>
                 `;
                 break;
             case 3:
-                infoContainer.textContent = 'Aquí irán tus habilidades y tecnologías que dominas.';
+                infoContainer.classList.add('skills-container');  // Añade una clase específica
+                infoContainer.innerHTML = `
+                <h2 class="info-title3">Soluciones Web Que Dejan Huella</h2>
+                <p class="skill-description">Desarrollamos soluciones que no solo cumplen con las expectativas, sino que las superan, asegurando resultados que realmente marcan la diferencia.</p>
+                <div class="skills-grid">
+                    <div class="skill-item">
+                        <img src="https://bnz07pap001files.storage.live.com/y4mFt0pGACUm4615Nth4cgI9djcZ4vipSkJe0eheIPIb1jqW3Z91thMF2i_B-6Jft1p31rRHzDxzMK7oQ6OzS7mc33GQfP1bp6qmtVCXUJlwuQ1FQEW-fMGKvc2bcGSflZNT3fpSwEN7ypbPcyh0ncteiDz30upIoCt_-X7qEu9JAgl3vYPHOboUl2ZhRONGPehnWiolTmOSUezBUQPQ6xhGjwue_940Z-9VjMUGjmBaXY?encodeFailures=1&width=575&height=397" alt="Skill 1">
+                        <h3 class="titleSkills">Tu Proyecto, Nuestra Prioridad</h3>
+                        <p>Soluciones web de alta calidad en tiempos récord, asegurando que tu negocio brille sin demoras.</p>
+                    </div>
+                    <div class="skill-item">
+                        <img src="https://bnz07pap001files.storage.live.com/y4mtA5zpVpIZt-NpcWjJzzJr1TmrMUlH-R7AUAuMaVVFannw7SFpw-skA_87NkE9naCWuggHZ2rOn7FkwYstzXUDNumaoXq7hobCO6YDPCySHAS_0lBpvmxksuQXtpT6dfGAYFDj2RncbuISzIu6qgCNucFWzIYI6LyFvRHl3Th8K4LSNRpdFeVuxz4zPottkh4dz_HiHFCa1ZnAGlzulpC6dj9oGYJcc8Saq0lYvS6M-E?encodeFailures=1&width=428&height=432" alt="Skill 2">
+                        <h3 class="titleSkills">SEO y Marketing</h3>
+                        <p>Optimizamos tu presencia online para maximizar tu alcance.</p>
+                    </div>
+                    <div class="skill-item">
+                        <img src="https://bnz07pap001files.storage.live.com/y4mdn5ioj77be15LTgoI1AZkGgo-_JM71YGz-QhNBXtMAzPsk-5uVhEGxT0FLTfwwvhxIyilzWaT7m7XXWgoIEG1De_LeTWcOQAI0Vn1Eno-rG36HhcPG2G0wz8es9LjhlWc8KumvCoxwkpSTDdymAtUNeiKbDuDwps8hD74VEx4HWNx1LZFyyzYJG9f1FbetA6Jw85PiXqtqwrG-pyGda90JeuN4n5oJuVQq8kyvOaPP8?encodeFailures=1&width=542&height=610" alt="Skill 3">
+                        <h3 class="titleSkills">E-commerce</h3>
+                        <p>Plataformas robustas para que vendas en línea sin complicaciones.</p>
+                    </div>
+                    <div class="skill-item">
+                        <img src="https://bnz07pap001files.storage.live.com/y4mk1MUEVyJReilNIiGtonH4QAe9NqOep_rEsYgwVtFpfH79RntTiUCqow1tju1_BYGJqklMRZIYatMYTnsj_7e6bqrl4vfyJ6yCXWwTtJtMXkrO-QzRL5_xSo2xsfc9kpoRYfzTbAOe7TQNWrUD0H_IkkgBYVy8DyZwm4Iff8J88KGZKTCvypUNcyVIz1YogTlPvWSFGTQF6L7yrBqcJcH7jk6Q4IVPm8UiBIAYvCB4Ao?encodeFailures=1&width=541&height=583" alt="Skill 4">
+                        <h3 class="titleSkills">Soporte Técnico</h3>
+                        <p>Mantenimiento y soporte técnico que mantiene tu sitio al 100%.</p>
+                    </div>
+                </div>
+                `;
                 break;
+            
         }
         
         inicioContainer.appendChild(infoContainer);
